d35ce979143cf653cbedff3b43cb9f8881c8f5bf
ChristosXeno <89767337+ChristosXeno@users.noreply.github.com>
2022-05-24 22:14:04 +1000

    Update xy2square.py
    Added code to the start of the function which converts pixel coordinates to board coordinates

--- assignment_1/scripts/xy2square.py
+++ assignment_1/scripts/xy2square.py
@@ -1,10 +1,25 @@
         
-### Function to convert chess square position (A1 to H8) to x,y
+### Function to convert x,y to chess square position (A1 to H8)
 from cv2 import threshold
 
-
-def xy2square(x,y):
+def xy2square(pix_x,pix_y):
         
+    # Define the size of the image (in pixels)
+    img_len_x = 640
+    img_len_y = 640
+    
+    # Define the board size (in m)
+    board_len_x = 0.48
+    board_len_y = 0.48
+    
+    # Define the bix to board ratio for x and y
+    pix2board_x = board_len_x/img_len_x
+    pix2board_y = -(board_len_y/img_len_y)
+    
+    # Convert the pixel to board coordinates
+    x = (pix_x * pix2board_x) - board_len_x
+    y = (pix_y * pix2board_y) + board_len_y
+    
     # 1-8, h-a
     # H1 is top left, A8 is bottom right
 
@@ -40,10 +55,16 @@ def xy2square(x,y):
     return square
 
 def main():
-
-    x = 0.21 - 7*0.06 
-    y = 0.44 + 7*0.06
-    print(xy2square(x,y))
+    
+    #x = 0.21 - 7*0.06 
+    #y = 0.44 + 7*0.06 
+    
+    
+    # Add pixel coordinates to test
+    pix_x = 0
+    pix_y = 0
+    
+    print(xy2square(pix_x,pix_y))
 
 
 if __name__ == '__main__':
